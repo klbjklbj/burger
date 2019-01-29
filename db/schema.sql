2277aa683ce0ee093de5e3f4e6f90481f12cd999
Kristen Johanson <43449427+klbjklbj@users.noreply.github.com>
2019-01-28 16:30:57 -0800

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,18 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(45) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
 );
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', 0);
