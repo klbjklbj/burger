53ed5ebb32480c7754bd68d86040e6c201d2a69e
Pele <bennett.johanson@gmail.com>
2019-01-29 10:03:32 -0800

    updated database schema & seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,14 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+DROP DATABASE IF EXISTS burgers_db;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(45) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
 );
